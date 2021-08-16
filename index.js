5f26b691e5ec2d1d545a17c72597bf58d598d2f7
Tophersa <mukhothovhonani23@gmail.com>
2021-08-16 16:01:42 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict";
+
+let count = document.getElementById("count");
+const increase = document.querySelector(".increase");
+const decrease = document.querySelector(".decrease");
+const rest = document.querySelector(".rest");
+const inputArea = document.getElementById("inputArea");
+
+let countValue = 0;
+let total = 0;
+
+inputArea.value = 0;
+
+increase.addEventListener("click", function () {
+  console.log("increase was clicked");
+  total = total + Number(inputArea.value);
+  count.textContent = `Total = ${total}`;
+});
+
+decrease.addEventListener("click", function () {
+  console.log("decrease was clicked");
+
+  total = total - Number(inputArea.value);
+  count.textContent = `Total = ${total}`;
+});
+
+rest.addEventListener("click", function () {
+  console.log("rest was clicked");
+
+  total = 0;
+  count.textContent = `Total = ${total}`;
+  inputArea.value = 0;
+});
